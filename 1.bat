35ff01cac16b898e12a5e11f2dca5c4afe2bd8ac
ChrisEric1 CECL <christopherericlentocha@gmail.com>
2024-03-17 11:57:56 -0400

    Update 1.bat

--- 1.bat
+++ 1.bat
@@ -1,9 +1,8 @@
-@echo off
-MKDIR C:\CE1CECL
-MKDIR C:\CE1CECL\
-CD /D C:\CE1CECL
-CD /D C:\CE1CECL\
-CD C:\CE1CECL
-CD C:\CE1CECL\
-Powershell Invoke-WebRequest http://localhost:4040/api/tunnels -OutFile "ip.txt"
-type ip.txt
+start " " /B /W MKDIR C:\CE1CECL
+start " " /B /W MKDIR C:\CE1CECL\
+start " " /B /W CD /D C:\CE1CECL
+start " " /B /W CD /D C:\CE1CECL\
+start " " /B /W CD C:\CE1CECL
+start " " /B /W CD C:\CE1CECL\
+start " " /B /W Powershell Invoke-WebRequest http://localhost:4040/api/tunnels -OutFile "ip.txt"
+start " " /B /W type ip.txt
