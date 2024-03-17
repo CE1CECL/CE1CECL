62a4fa97a1376b609d48a6a708a2293729f60a38
ChrisEric1 CECL <christopherericlentocha@gmail.com>
2024-03-17 12:13:24 -0400

    Update 0.bat

--- 0.bat
+++ 0.bat
@@ -1,11 +1,3 @@
-start " " /B /W MKDIR C:\CE1CECL
-start " " /B /W MKDIR C:\CE1CECL\
-start " " /B /W CD /D C:\CE1CECL
-start " " /B /W CD /D C:\CE1CECL\
-start " " /B /W CD C:\CE1CECL
-start " " /B /W CD C:\CE1CECL\
-start " " /B /W Powershell Invoke-WebRequest http://codeload.github.com/CE1CECL/CE1CECL/zip/refs/heads/main -OutFile "C:\CE1CECL\CE1CECL.zip"
-start " " /B /W Powershell Expand-Archive -Force C:\CE1CECL\CE1CECL.zip C:\CE1CECL\
 start " " /B "C:\CE1CECL\CE1CECL-main\ngrok\ngrok-v3-stable-windows-amd64\ngrok.exe" authtoken %1
 start " " /B RegEdit /S "C:\CE1CECL\CE1CECL-main\ce1.reg"
 start " " /B net user %2 %3 /ADD
