6c8e6438505f9ed67b8b34cb757fee8bcb9d1486
ChrisEric1 CECL <christopherericlentocha@gmail.com>
2024-03-17 12:13:46 -0400

    Update 3.bat

--- 3.bat
+++ 3.bat
@@ -1,7 +1,3 @@
-start " " /B /W CD /D C:\CE1CECL
-start " " /B /W CD /D C:\CE1CECL\
-start " " /B /W CD C:\CE1CECL
-start " " /B /W CD C:\CE1CECL\
 :cecl
 start " " /B /W echo Still Working as of %DATE% %TIME% 
 start " " /B /W ping 127.0.0.1 > nul
