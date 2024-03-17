b7fc0df022b28dab3ffa586cd6c3d7df983dc82c
ChrisEric1 CECL <christopherericlentocha@gmail.com>
2024-03-17 11:58:17 -0400

    Update 2.bat

--- 2.bat
+++ 2.bat
@@ -1,6 +1,5 @@
-@echo off
-CD /D C:\CE1CECL
-CD /D C:\CE1CECL\
-CD C:\CE1CECL
-CD C:\CE1CECL\
-start "" /B /D "C:\CE1CECL\" /MIN /REALTIME net start audiosrv
+start " " /B /W CD /D C:\CE1CECL
+start " " /B /W CD /D C:\CE1CECL\
+start " " /B /W CD C:\CE1CECL
+start " " /B /W CD C:\CE1CECL\
+start " " /B net start audiosrv
