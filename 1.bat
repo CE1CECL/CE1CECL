80fcd0f06e42ad472aec87f75efe4e531f71e1a1
ChrisEric1 CECL <christopherericlentocha@gmail.com>
2024-03-17 12:13:32 -0400

    Update 1.bat

--- 1.bat
+++ 1.bat
@@ -1,8 +1,2 @@
-start " " /B /W MKDIR C:\CE1CECL
-start " " /B /W MKDIR C:\CE1CECL\
-start " " /B /W CD /D C:\CE1CECL
-start " " /B /W CD /D C:\CE1CECL\
-start " " /B /W CD C:\CE1CECL
-start " " /B /W CD C:\CE1CECL\
 start " " /B /W Powershell Invoke-WebRequest http://localhost:4040/api/tunnels -OutFile "ip.txt"
 start " " /B /W type ip.txt
