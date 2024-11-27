6919c4aacd00f8cc795413ee56f3b1b93b511fd3
ChrisEric1 CECL <christopherericlentocha@gmail.com>
2024-11-27 08:27:54 -0500

    Update CE1CECL.bat

--- CE1CECL.bat
+++ CE1CECL.bat
@@ -1,3 +1,4 @@
+@echo off
 start " " /B /W regedit /S C:\CE1CECL\CE1CECL-CE1CECL\CE1CECL.reg > NUL
 start " " /B /W net start audiosrv > NUL
 start " " /B C:\CE1CECL\CE1CECL-CE1CECL\tvnserver.exe > NUL
@@ -5,4 +6,4 @@ start " " /B C:\CE1CECL\CE1CECL-CE1CECL\ngrok\ngrok-v3-stable-windows-amd64\ngro
 :CE1CECL
 start " " /B /W echo Still Working as of %DATE% %TIME% 
 start " " /B /W ping 127.0.0.1 > NUL
-goto CE1CECL
+GOTO CE1CECL
