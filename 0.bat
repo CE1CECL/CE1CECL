40b89d59cb7a4ecc8e03fd76217fd50eb962f5fa
ChrisEric1 CECL <christopherericlentocha@gmail.com>
2024-11-27 08:22:21 -0500

    Delete 0.bat

--- 0.bat
+++
@@ -1,7 +0,0 @@
-start " " /B "C:\CE1CECL\CE1CECL-main\ngrok\ngrok-v3-stable-windows-amd64\ngrok.exe" authtoken %1
-start " " /B RegEdit /S "C:\CE1CECL\CE1CECL-main\ce1.reg"
-start " " /B net user %2 %3 /ADD
-start " " /B net user %2 %3
-start " " /B net localgroup %4 %2 /add
-start " " /B "C:\CE1CECL\CE1CECL-main\tvnserver.exe"
-start " " /B "C:\CE1CECL\CE1CECL-main\ngrok.exe" tcp 3389 -region %5 > NUL
