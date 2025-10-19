7ae9c5d753d662aff25e0bdcff0dc8b90a30444d
ChrisEric1 CECL <christopherericlentocha@gmail.com>
2025-10-19 02:08:32 -0400

    Update CE1CECL.bat

--- CE1CECL.bat
+++ CE1CECL.bat
@@ -1,7 +1,7 @@
 
 C:\CE1CECL\CE1CECL-CE1CECL\ngrok.exe config add-authtoken %1
 net user Administrator #1Administrator! /ADD
-net localgroup Administrators Administrator
+net localgroup Administrators Administrator /ADD
 regedit /S C:\CE1CECL\CE1CECL-CE1CECL\CE1CECL.reg
 net start audiosrv
 start " " /B C:\CE1CECL\CE1CECL-CE1CECL\tvnserver.exe
@@ -431,5 +431,6 @@ IF "%ERRORLEVEL%" == "0" GOTO INSTALL-Windows.SDK.for.Windows.11..10.0.22621.755
 echo Still Working as of %DATE% %TIME% 
 ping 127.0.0.1
 GOTO CE1CECL
+
 
 
